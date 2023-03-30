9b9d2d508dd7c8f53969cadb783b92d2ba58fedc
Stangovic <kataneisaac@gmail.com>
2023-03-30 04:22:16 -0700

    Singly linked list project

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* list_len - returns the number of elements in a linked list
+* @h: pointer to the struct linked list
+* Return: the number of elements
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h)
+	{
+		h = h->next;
+		elements += 1;
+	}
+
+	return (elements);
+}
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* printFirst - prints before main is exectuted
+*/
+
+void printFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
