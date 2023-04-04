3ca06a8e20f25a860a7c7502dc3cd90012214caf
Stangovic <kataneisaac@gmail.com>
2023-04-04 05:09:59 -0700

    Task02

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,25 +1,20 @@
-Here's a possible implementation of the `add_nodeint` function in C:
-
-```c
-#include <stdlib.h>
 #include "lists.h"
 
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+/**
+ * add_nodeint - Add node at beggining
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *naw;
 
-listint_t *add_nodeint(listint_t **head, const int n) {
-    listint_t *new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL) {
-        return NULL;  // allocation failed
-    }
-    new_node->n = n;
-    new_node->next = *head;
-    *head = new_node;
-    return new_node;
+	naw = malloc(sizeof(listint_t));
+	if (naw == NULL)
+		return (NULL);
+
+	naw->n = n;
+	naw->next = *head;
+
+	*head = naw;
+
+	return (naw);
 }
-```
-
-This function takes a pointer to the head of the list (`head`) and an integer value (`n`) as arguments. It allocates memory for a new `listint_t` node using `malloc`, checks if the allocation was successful, sets the value of the `n` member to `n`, sets the `next` pointer of the new node to the current head of the list (`*head`), and finally updates the `head` pointer to point to the new node. It returns a pointer to the new node, or `NULL` if the allocation failed.
-PS I love you. And i asked the Ask AI app to write this for me. Get it for free --> https://get-askai.app
