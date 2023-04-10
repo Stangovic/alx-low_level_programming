1a601fd8d11a37eb852c629a83344f763ba4478a
Stangovic <kataneisaac@gmail.com>
2023-04-10 02:48:01 -0700

    Task 2.1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-int get_bit(unsigned long int n, unsigned int index) {
+int get_bit(unsigned long int n, unsigned int index) 
+{
     if (index >= sizeof(unsigned long int) * 8) {
-        return -1;  // index out of range
+        return -1;
     }
-    unsigned long int mask = 1ul << index;
-    return (n & mask) != 0;
+    return (n >> index) & 1;
 }
