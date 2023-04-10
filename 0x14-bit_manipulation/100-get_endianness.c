80e1927f1c04bf8ead8c093789bfb62409586fd9
Stangovic <kataneisaac@gmail.com>
2023-04-10 05:04:54 -0700

    fix task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,9 +5,10 @@ int get_endianness(void)
     int num = 1;
     char *endian = (char *)&num;
 
-    if (*endian == 1) {
-        return 1; // Little Endian
+    if (*endian == 1) 
+    {
+        return 1;
     }
 
-    return 0; // Big Endian
+    return 0;
 }
