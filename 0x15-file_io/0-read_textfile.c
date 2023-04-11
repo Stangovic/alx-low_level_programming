862bab4fa5c8236658f64a179adbd23833509af6
Stangovic <kataneisaac@gmail.com>
2023-04-10 22:04:06 -0700

    Fix task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-    int fd;
-    ssize_t bytes_read, bytes_written;
+ssize_t read_textfile(const char *filename, size_t letters) {
+    int file_descriptor, bytes_read, bytes_written;
     char *buffer;
 
-    if (filename == NULL)
-        return -1;
-
-    buffer = malloc(letters);
-    if (buffer == NULL)
-        return -1;
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        free(buffer);
-        return -1;
+    if (filename == NULL) {
+        return 0;
     }
 
-    bytes_read = read(fd, buffer, letters);
+    buffer = (char*) malloc(sizeof(char) * letters);
+    if (buffer == NULL) {
+        return 0;
+    }
+
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1) {
+        free(buffer);
+        return 0;
+    }
+
+    bytes_read = read(file_descriptor, buffer, letters);
     if (bytes_read == -1) {
         free(buffer);
-        close(fd);
-        return -1;
+        close(file_descriptor);
+        return 0;
     }
 
     bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
     if (bytes_written == -1 || bytes_written != bytes_read) {
         free(buffer);
-        close(fd);
-        return -1;
+        close(file_descriptor);
+        return 0;
     }
 
     free(buffer);
-    close(fd);
+    close(file_descriptor);
 
     return bytes_written;
 }
