17dcae6eba14f64b0b9b5612569e671361046e2b
Stangovic <kataneisaac@gmail.com>
2023-04-11 00:25:56 -0700

    More fixes to task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int append_text_to_file(const char *filename, const char *text_content) 
-{
+int append_text_to_file(const char *filename, char *text_content) {
     if (filename == NULL) {
         return -1;
     }
 
-    int file_descriptor = open(filename, O_WRONLY | O_APPEND);
-    if (file_descriptor == -1) {
+    FILE *file = fopen(filename, "a");
+    if (file == NULL) {
         return -1;
     }
 
-    size_t text_length = strlen(text_content);
-    ssize_t bytes_written = write(file_descriptor, text_content, text_length);
-    if (bytes_written == -1 || (size_t) bytes_written != text_length) {
-        return -1;
+    if (text_content != NULL) {
+        fprintf(file, "%s", text_content);
     }
 
-    if (close(file_descriptor) == -1) {
-        return -1;
-    }
-
-    return 0;
+    fclose(file);
+    return 1;
 }
