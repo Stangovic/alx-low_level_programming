f6fe5827601aee4848aaaabe06efda2f9d1640ad
Stangovic <kataneisaac@gmail.com>
2023-04-10 23:02:51 -0700

    More fix to task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,27 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
-int create_file(const char *filename, const char *text_content) 
+int create_file(const char *filename, char *text_content) 
 {
     if (filename == NULL) {
         return -1;
     }
 
-    int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
     if (fd == -1) {
         return -1;
     }
 
-    ssize_t nwritten = 0;
     if (text_content != NULL) {
-        while (text_content[nwritten] != '\0') {
-            nwritten++;
+        size_t len = strlen(text_content);
+        ssize_t nwritten = write(fd, text_content, len);
+        if (nwritten == -1 || (size_t)nwritten != len) {
+            close(fd);
+            return -1;
         }
-        nwritten = write(fd, text_content, nwritten);
     }
 
     close(fd);
-    return nwritten == -1 ? -1 : 0;
+    return 1;
 }
