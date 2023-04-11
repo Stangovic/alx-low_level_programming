b3fcb7e9f55d052e25de08c24446aee922fd5afd
Stangovic <kataneisaac@gmail.com>
2023-04-11 00:08:19 -0700

    rewrite task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,32 @@
 #include "main.h"
-#include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-int create_file(const char *filename, char *text_content) 
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer 
+ * @text_content: create a file
+ *
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL) {
-        return -1;
-    }
+	int a, w, sin = 0;
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1) {
-        return -1;
-    }
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL) {
-        size_t len = strlen(text_content);
-        ssize_t nwritten = write(fd, text_content, len);
-        if (nwritten == -1 || (size_t)nwritten != len) {
-            close(fd);
-            return -1;
-        }
-    }
+	if (text_content != NULL)
+	{
+		for (sin = 0; text_content[sin];)
+			sin++;
+	}
 
-    close(fd);
-    return 1;
-}
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(a, text_content, sin);
+
+	if (a == -1 || w == -1)
+		return (-1);
+
+	close(a);
+
+	return (1);
+}	
